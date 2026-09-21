e035a9697f2d56c26877a0d70c427a445753656c
Garima Purohit <purohitgarima2307@gmail.com>
2026-09-21 23:07:42 +0530

    commit changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRFIHt0lyn1f2mkoYqK-5vpveZfEu74x4",
-  authDomain: "cafenova-a4b0b.firebaseapp.com",
-  projectId: "cafenova-a4b0b",
-  storageBucket: "cafenova-a4b0b.firebasestorage.app",
-  messagingSenderId: "565202652072",
-  appId: "1:565202652072:web:981b92c3eeb000a31b2b9d",
-  measurementId: "G-MHY8ZSNW9F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export {auth , app};
 
+export { auth, app };
